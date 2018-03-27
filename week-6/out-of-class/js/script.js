b42f5c148d37bd328c11d80fb368cf28cc8236dd
morine <morine@simmons.edu>
2018-03-27 19:19:59 -0400

    dailyHours() assignment

--- week-6/out-of-class/js/script.js
+++ week-6/out-of-class/js/script.js
@@ -4,8 +4,8 @@ function renderOutput(output) {
     let output_element = document.getElementById("output");
     output_element.innerHTML += "<div>" + output + "</div>"
 }
-function dailyHours(hours) 
- hours = [
+
+let  hours = [
     "Monday: 9a - 9p",
     "Tuesday: 9a - 9p",
     "Wednesday: 9a - 9p",
@@ -13,21 +13,23 @@ function dailyHours(hours)
     "Friday: 9a - 5p",
     "Saturday: 9a - 5p",
     "Sunday: closed"
-]
+];
+function displayOneDaysHours(one_days_hours) {
+  let parts_of_hours = one_days_hours.split(": ");
+  
+   if (parts_of_hours[0] === "Sunday") { 
+     renderOutput( "On Sunday the Library is Closed" )
+                  
+}else {
 
-let original_name = 
-    "Monday: 9a - 9p",
-    "Tuesday: 9a - 9p",
-    "Wednesday: 9a - 9p",
-    "Thursday: 9a - 9p",
-    "Friday: 9a - 5p",
-    "Saturday: 9a - 5p",
-    "Sunday: closed";
-let parts_of_name = original_string.split(":");
-console.log(parts_of_string[0], parts_of_string[1]);
-if (dailyHours[1] == "closed") {
-    renderOutput ("On " + dailyHours[0] + " the library is " + dailyHours[1]);
- }
- else {
-  renderOutput ("On " + dailyHours[0] + " the hours are " + dailyHours[1]);
+  renderOutput("On " + parts_of_hours [0] + " the hours are " + parts_of_hours[1]);
   }
+}
+  
+  
+  
+
+ 
+
+
+
